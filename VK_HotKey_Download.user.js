898cbce23f69d7578b62a8aa74b127e21214bcb3
kapsilon <kapsilon47@gmail.com>
2020-05-20 14:31:29 +0300

    Two new variants to VK

--- VK_HotKey_Download.user.js
+++ VK_HotKey_Download.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		VK HotKey Download
 // @namespace	https://vash.omnimir.ru/
-// @version		0.2
+// @version		0.3
 // @description	Press DOWN to DOWNload
 // @author		kapsilon
 // @license		MIT
@@ -27,6 +27,19 @@
 				//Third style choice
 				if (document.querySelector(".flat_button.fl_r")) {
 					document.querySelector(".flat_button.fl_r").click();
+				}
+				//Fourth style choice
+				if (document.querySelector(".thumbed_link a")) {
+					document.querySelector(".thumbed_link a").click();
+				}
+				//Fifth style choice
+				if (document.querySelector(".article_decoration_before a")) {
+					//Maybe two links on page
+					let downlinks = document.querySelectorAll(".article_decoration_before a");
+					//Choosing last link
+					if (downlinks) {
+						downlinks[downlinks.length - 1].click();
+					}
 				}
 			}
 		},
