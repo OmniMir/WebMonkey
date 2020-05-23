5bf57831475943112d5c9fcbe9613a3135325748
kapsilon <kapsilon47@gmail.com>
2020-05-23 14:13:38 +0300

    Kaspersky Hide Shields Waiting

--- Kaspersky_Hide_Shields.user.js
+++ Kaspersky_Hide_Shields.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		Kaspersky_Hide_Shields
 // @namespace	https://vash.omnimir.ru/
-// @version		0.1
+// @version		0.2
 // @description	Hide annoying green shields from search
 // @author		kapsilon
 // @license		MIT
@@ -15,10 +15,13 @@
 	window.addEventListener(
 		"load",
 		function () {
-			//Search special image by name and hide all of them
-			document.querySelectorAll("img[name=kl_1590231308987]").forEach((element) => {
-				element.style.display = "none";
-			});
+			//Waiting to Kapsersky Protection Extension
+			setTimeout(() => {
+				//Search special image by name and hide all of them
+				document.querySelectorAll("img[name=kl_1590231308987]").forEach((element) => {
+					element.style.display = "none";
+				});
+			}, 2000);
 		},
 		false
 	);
