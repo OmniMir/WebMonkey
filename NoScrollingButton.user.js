17630fdf946ff7b8e35d20112a515aa47f7802cf
kapsilon <kapsilon47@gmail.com>
2020-05-23 10:27:56 +0300

    Ass No Scrolling Button
    Inspireв by No Smooth Scrolling Extension

---
+++ NoScrollingButton.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name		No Scrolling Button
+// @namespace	https://vash.omnimir.ru/
+// @version		0.1
+// @description	Disable Smooth Scroll by Middle Mouse Button
+// @author		kapsilon
+// @license		MIT
+// @match		http://*/*
+// @match		https://*/*
+// @grant		none
+// ==/UserScript==
+
+(function () {
+	"use strict";
+	window.addEventListener(
+		"mousedown",
+		function (mouseEvent) {
+			if (mouseEvent.button == 1) {
+				mouseEvent.preventDefault();
+				mouseEvent.stopPropagation();
+			}
+		},
+		true
+	);
+})();
