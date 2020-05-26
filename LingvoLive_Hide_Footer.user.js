923d1c3f632a5b2dec0ad82710a0cb616eb47a6d
kapsilon <kapsilon47@gmail.com>
2020-05-26 12:30:36 +0300

    LingvoLive Footer Update
    Simple solution with scroll
    If you see footer, you have scrolled, yes

--- LingvoLive_Hide_Footer.user.js
+++ LingvoLive_Hide_Footer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		LingvoLive Hide Footer
 // @namespace	https://vash.omnimir.ru/
-// @version		0.5
+// @version		0.6
 // @description	Hide that big black footer
 // @author		kapsilon
 // @license		MIT
@@ -15,6 +15,14 @@
 	//Start on page load to take effect
 	window.addEventListener(
 		"load",
+		function () {
+			wmHide("._3nyLP");
+		},
+		false
+	);
+	//Going with page scroll
+	window.addEventListener(
+		"scroll",
 		function () {
 			wmHide("._3nyLP");
 		},
