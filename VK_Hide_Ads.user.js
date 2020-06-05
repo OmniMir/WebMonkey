b9827392d52b5af4e5cecf7f6a56dab0f978f3fd
kapsilon <kapsilon47@gmail.com>
2020-06-05 15:43:00 +0300

    Add Hide Ad at VK

---
+++ VK_Hide_Ads.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name		VK Hide Ads
+// @namespace	https://vash.omnimir.ru/
+// @version		0.6
+// @description	Hide some advertisements on VK
+// @author		kapsilon
+// @license		MIT
+// @match		https://vk.com/*
+// @require		https://raw.githubusercontent.com/OmniMir/WebMonkey/master/lib.min.js
+// @grant		none
+// ==/UserScript==
+
+(function () {
+	"use strict";
+	//Start on page load to take effect
+	window.addEventListener(
+		"load",
+		function () {
+			//Remove ads from left menu
+			wmRemove("#ads_left");
+			//Remove ads from feed
+			wmRemove(".ads_ad_box");
+		},
+		false
+	);
+	//Remove ads from feed with scrolling
+	window.addEventListener(
+		"scroll",
+		function () {
+			wmRemove(".ads_ad_box");
+		},
+		false
+	);
+})();
