21321619f8eb6f9a2fd31ac17df5539de4c3acee
kapsilon <kapsilon47@gmail.com>
2020-06-05 15:45:58 +0300

    Add Notification Hide on ucoz sites

---
+++ uCozSites_Hide_Notification.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name		uCoz Sites Hide Notification
+// @namespace	https://vash.omnimir.ru/
+// @version		0.6
+// @description	Hide annoying fake notification from all uCoz sites
+// @author		kapsilon
+// @match		http://*.ucoz.com/*
+// @match		http://*.ucoz.org/*
+// @match		http://*.ucoz.ru/*
+// @require		https://raw.githubusercontent.com/OmniMir/WebMonkey/master/lib.min.js
+// @grant		none
+// ==/UserScript==
+
+(function () {
+	"use strict";
+	//Start on page load to take effect
+	window.addEventListener(
+		"load",
+		function () {
+			wmHide(".pshsndr");
+		},
+		false
+	);
+})();
