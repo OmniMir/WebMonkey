3fa4c94932b28859cf53c8de3408392de310c5b5
kapsilon <kapsilon47@gmail.com>
2020-06-05 15:27:20 +0300

    Add KinoPoisk Scripts

---
+++ KinoPoisk_Hide_Banners.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name		KinoPoisk Hide Banners
+// @namespace	https://vash.omnimir.ru/
+// @version		0.6
+// @description	Hide Big Banners
+// @author		kapsilon
+// @license		MIT
+// @match		https://www.kinopoisk.ru/*
+// @require 	https://raw.githubusercontent.com/OmniMir/WebMonkey/master/lib.min.js
+// @grant		none
+// ==/UserScript==
+
+(function () {
+	"use strict";
+	//Start on page load to take effect
+	window.addEventListener(
+		"load",
+		function () {
+			//After filmcard banner
+			wmRemove("div[data-metrika=film_card] + *");
+			//Right side banner
+			wmRemove("div[data-type=teaserspec] + *");
+            wmRemove(".inviteFriends + *");
+			//Hovering corner banner
+			wmRemove(".promo.banner__area");
+			//Top banner
+			wmWaitAndRemove("div[style='min-height: 0px;']");
+			wmWaitWaitAndRemove("div[style='min-height: 0px;']");
+		},
+		false
+	);
+})();
