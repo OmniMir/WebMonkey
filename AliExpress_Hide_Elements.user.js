68202e2103256948454fc3b7207dbc7e88b1fb62
kapsilon <kapsilon47@gmail.com>
2020-06-28 21:15:06 +0300

    REFACTOR AliExpress to universal Hide_Elements

--- AliExpress_Hide_Elements.user.js
+++ AliExpress_Hide_Elements.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name		AliExpress Hide Social
+// @name		AliExpress Hide Elements
 // @namespace	https://vash.omnimir.ru/
-// @version		0.6
-// @description	Hide Social Buttons
+// @version		0.7
+// @description	Hide annoying elements
 // @author		kapsilon
 // @license		MIT
 // @match		https://*.aliexpress.com/*
@@ -17,7 +17,16 @@
 	window.addEventListener(
 		"load",
 		function () {
+			//HideTop Banners
+			wmHide(".top-banner-container");
+			wmWaitAndHide("a[data-spm-anchor-id]");
+			//Hide Social Buttons
 			wmHide(".product-fix-sns");
+			//Hide Footer
+			wmHide(".footer-copywrite");
+			wmHide(".site-footer");
+			//Hide Eva
+			wmWaitAndHide("#J_xiaomi_dialog");
 		},
 		false
 	);
