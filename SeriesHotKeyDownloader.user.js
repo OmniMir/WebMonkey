ed925cb6c01650d4d42ad3f8d7d00f28fc2ae2d9
kapsilon <kapsilon47@gmail.com>
2020-06-28 20:47:22 +0300

    Universal SeriesHotKeyDownloader

--- SeriesHotKeyDownloader.user.js
+++ SeriesHotKeyDownloader.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
-// @name		LostFilm HotKey Download
+// @name		TEST Series HotKey Downloader
 // @namespace	https://vash.omnimir.ru/
-// @version		0.6
+// @version		0.7
 // @description	Press DOWN to DOWNload
 // @author		kapsilon
-// @match		https://www.lostfilm.tv/*
+
+// @match		http://baibako.tv/*
 // @match		http://insearch.site/*
+// @match		https://www.lostfilm.tv/*
 // @require		https://raw.githubusercontent.com/OmniMir/WebMonkey/master/lib.min.js
 // @grant		none
 // ==/UserScript==
@@ -17,9 +19,11 @@
 		"keydown",
 		function (event) {
 			if (event.code == "ArrowDown") {
-				//LostfiLm Site
+				//Baibako
+				wmClick(".fa-download");
+				//LostFilm
 				wmClickLast(".external-btn");
-				//Insearch Site
+				//LostFilm Insearch
 				wmClickNumber(".inner-box--link.main a", 2);
 				wmClickNumber(".block_3 a", 2);
 			}
