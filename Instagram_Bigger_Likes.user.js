18877820c30312864c27ef3e2d2195f404e45745
kapsilon <kapsilon47@gmail.com>
2020-07-03 18:30:22 +0300

    Instagram_Bigger_Likes use lib now

--- Instagram_Bigger_Likes.user.js
+++ Instagram_Bigger_Likes.user.js
@@ -1,24 +1,22 @@
 // ==UserScript==
 // @name		Instagram Bigger Likes
 // @namespace	https://vash.omnimir.ru/
-// @version		0.1
+// @version		0.2
 // @description	Numbaers can be BIG
 // @author		kapsilon
 // @license		MIT
 // @match		https://www.instagram.com/*
+// @require		https://raw.githubusercontent.com/OmniMir/WebMonkey/master/lib.min.js
 // @grant		none
 // ==/UserScript==
 
 (function () {
 	"use strict";
 	//Add new style for existing class
-	let bigLikes = document.createElement("style");
-	bigLikes.type = "text/css";
-	bigLikes.innerText = `
+	wmAddNewStyle(`
 			.-V_eO {
 			font-size: 50px;
 			line-height: 50px;
 			}
-		`;
-	document.head.appendChild(bigLikes);
+		`);
 })();
