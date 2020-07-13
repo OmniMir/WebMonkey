705dee3440c6531aba7d7f1db7def6c9c2017af2
kapsilon <kapsilon47@gmail.com>
2020-07-13 15:15:24 +0300

    FIX KinoPoisk_Hide_Elements for new design
    ALL NEW STYLE

--- KinoPoisk_Hide_Elements.user.js
+++ KinoPoisk_Hide_Elements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		KinoPoisk Hide Elements
 // @namespace	https://vash.omnimir.ru/
-// @version		0.7
+// @version		0.8
 // @description	Hide annoying elements
 // @author		kapsilon
 // @license		MIT
@@ -17,40 +17,26 @@
 		"load",
 		function () {
 			//Hide Streaming Theater
-			wmRemove("._2lIaVuplclqoEPxCO_yWsY._3tmz2UPcIDTjaP7bG35GSc");
-			wmRemove(".movie-info__button-container");
+			wmHide(".kinopoisk-header-featured-menu");
 			//Hide Top banner
-			wmWaitAndRemove("div[style='min-height: 0px;']");
-			wmWaitWaitAndRemove("div[style='min-height: 0px;']");
+			wmHide("div[class^=styles_themeTopBanner]");
 			//Hide Add to Collection
-			wmRemove(".addFolder");
-			wmRemove("#div_mustsee_main");
-			wmRemove(".film-img-box__soundtrack");
-			wmRemove(".to_friend");
-			//Hide IMAX icon
-			wmRemove(".imax-item");
-			//Hide After filmcard banner
-			wmRemove("div[data-metrika=film_card] + *");
-			wmRemove("div[class^=view_people__]");
+			wmHide("button[class^=styles_toWatchButton]");
+			wmHide("div[class^=styles_userControlsContainer]");
 			//Hide Social buttons
-			wmRemove(".block_social");
-			wmRemove(".new-share-buttons");
+			wmHide("#film-share-buttons");
+			//Hide IMAX icon
+			wmHide("div[class^=styles_imaxContainer]");
+			//Hide Ads
+			wmHideAll("div[class*=styles_rootRendered]");
 			//Hide Recommended movies
-			wmRemove("#film_recom");
-			//Hide Right side banner
-			wmRemove("div[data-type=teaserspec] + *");
-			wmRemove(".inviteFriends + *");
-			//Hide Subscribe feature
-			wmRemove(".subscribe");
-			//Hide Hovering corner banner
-			wmRemove(".promo.banner__area");
-			//Hide Movie Ads
-			wmRemove("#partial_component__watch-online-person-slider");
-			//Hide Annoying RandomMovie
-			wmHide(".randomMovie");
+			wmHide(".similar-films-block");
+			//Hide Trailers (go to Trailers Page)
+			wmHide(".film-trailers-block");
+			//Hide Movie Lists
+			wmHide(".styles_root__V9fG5");
 			//Hide Footer
-			wmHide("#partial_component__footer");
-			wmHide("#footer_wrapper");
+			wmHide("div[class^=styles_footerContainer_]");
 		},
 		false
 	);
