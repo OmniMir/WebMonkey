e0296906feab804426e53d87295e0a590967215a
kapsilon <kapsilon47@gmail.com>
2020-07-14 04:12:43 +0300

    HOTFIX return old code for old design
    Some pages are new
    Some pages are old
    You hide all of them

--- KinoPoisk_Hide_Elements.user.js
+++ KinoPoisk_Hide_Elements.user.js
@@ -16,6 +16,7 @@
 	window.addEventListener(
 		"load",
 		function () {
+			//FOR NEW DESIGN
 			//Hide Streaming Theater
 			wmHide(".kinopoisk-header-featured-menu");
 			wmHide(".hd-nameplate");
@@ -53,6 +54,43 @@
 			wmHide(".styles_root__V9fG5");
 			//Hide Footer
 			wmHide("div[class^=styles_footerContainer_]");
+
+			//FOR OLD DESIGN
+			//Hide Streaming Theater
+			wmRemove("._2lIaVuplclqoEPxCO_yWsY._3tmz2UPcIDTjaP7bG35GSc");
+			wmRemove(".movie-info__button-container");
+			//Hide Top banner
+			wmWaitAndRemove("div[style='min-height: 0px;']");
+			wmWaitWaitAndRemove("div[style='min-height: 0px;']");
+			//Hide Add to Collection
+			wmRemove(".addFolder");
+			wmRemove("#div_mustsee_main");
+			wmRemove(".film-img-box__soundtrack");
+			wmRemove(".to_friend");
+			//Hide IMAX icon
+			wmRemove(".imax-item");
+			//Hide After filmcard banner
+			wmRemove("div[data-metrika=film_card] + *");
+			wmRemove("div[class^=view_people__]");
+			//Hide Social buttons
+			wmRemove(".block_social");
+			wmRemove(".new-share-buttons");
+			//Hide Recommended movies
+			wmRemove("#film_recom");
+			//Hide Right side banner
+			wmRemove("div[data-type=teaserspec] + *");
+			wmRemove(".inviteFriends + *");
+			//Hide Subscribe feature
+			wmRemove(".subscribe");
+			//Hide Hovering corner banner
+			wmRemove(".promo.banner__area");
+			//Hide Movie Ads
+			wmRemove("#partial_component__watch-online-person-slider");
+			//Hide Annoying RandomMovie
+			wmHide(".randomMovie");
+			//Hide Footer
+			wmHide("#partial_component__footer");
+			wmHide("#footer_wrapper");
 		},
 		false
 	);
