f77ef76213a5039b9ce7957e542ec10ea935349e
kapsilon <kapsilon47@gmail.com>
2020-10-11 02:22:08 +0300

    AutoCloser Add reloading

--- AutoCloser.user.js
+++ AutoCloser.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name		AutoCloser
 // @namespace	https://vash.omnimir.ru/
-// @version		1.0
+// @version		1.1
 // @description	Close annoying useless auto-opened web-pages
 // @author		kapsilon
 // @license		MIT
 // @match		http://playinfo.gomlab.com/ending_browser.gom?product=GOMPLAYER
-// @match		https://www.kcsoftwares.com/?page=postinstall&sw=SUMo
+// @match		http://www.gomtv.com/gomtvplus/
+// @match		https://www.kcsoftwares.com/?page=postinstall&sw=*
 // @match		https://vash.omnimir.ru/tabautoclose/
 // @grant		window.close
 // @run-at		document-start
@@ -17,4 +18,9 @@
 	//@run-at used for acceleration
 	//That just works (only with @grant)
 	window.close();
+
+	//Reload if not closed
+	setTimeout(() => {
+		location.reload()
+	}, 1000);
 })();
