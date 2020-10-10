a4e636a4f2dbd0e431a05a3402603a09664fd49e
kapsilon <kapsilon47@gmail.com>
2020-10-11 02:45:10 +0300

    Sites_HotKey_Previous Add New Sites
    AliExpress
    ChookAndGeek
    ComicVine
    Google Search
    Lifehacker
    MailRu
    NarutoBase
    OzonPWNews
    Repack

--- Sites_HotKey_Previous.user.js
+++ Sites_HotKey_Previous.user.js
@@ -4,9 +4,22 @@
 // @version		0.6
 // @description	Press LEFT/RIGHT to CONTROL
 // @author		kapsilon
+// @match		https://aliexpress.com/*
+// @match		https://aliexpress.ru/*
+// @match		https://*.aliexpress.com/*
+// @match		https://*.aliexpress.ru/*
 // @match		https://animejoy.ru/*
 // @match		https://baibako.tv/*
+// @match		https://www.chookandgeek.ru/*
 // @match		https://comicsdb.ru/*
+// @match		https://comicvine.gamespot.com/*
+// @match		https://www.google.com/search*
+// @match		https://*.mail.ru/*
+// @match		https://lifehacker.ru/*
+// @match		https://naruto-base.su/*
+// @match		https://www.ozon.ru/*
+// @match		https://pwnews.net/news/*
+// @match		https://repack.me/*
 // @require 	https://raw.githubusercontent.com/OmniMir/WebMonkey/master/lib.min.js
 // @grant		none
 // ==/UserScript==
@@ -20,13 +33,33 @@
 			if (event.code == "ArrowLeft") {
 				//Disable Default Action
 				event.preventDefault();
+
+				//AliExpress
+				wmClick(".next-pagination-item.next-prev");
+				wmClick(".ui-pagination-prev");
 				//AnimeJoy
 				wmClick(".page_prev  a");
 				//Baibako
 				wmClick(".index a");
+				//ChookAndGeek & Repack
+				wmClick(".pagination a");
 				//ComicsDB
 				wmClick(".previous a");
 				wmClick("small a");
+				//ComicVine
+				wmClick(".paginate__item.prev a");
+				//Google
+				wmClick("#pnprev");
+				//Lifehacker
+				wmClick(".the-paginator__arrow-icon--left");
+				//MailRu
+				wmClick(".icon_triangle_left");
+				//NarutoBase
+				wmClick("a.series-link");
+				//Ozon
+				//NONE
+				//PWNews
+				wmClick(".catPages1 a");
 			}
 		},
 		false
@@ -38,13 +71,33 @@
 			if (event.code == "ArrowRight") {
 				//Disable Default Action
 				event.preventDefault();
+
+				//AliExpress
+				wmClick(".next-pagination-item.next-next");
+				wmClick(".ui-pagination-next");
 				//AnimeJoy
 				wmClick(".page_next  a");
 				//Baibako
 				wmClickLast(".index a");
+				//ChookAndGeek & Repack
+				wmClickLast(".pagination a");
 				//ComicsDB
 				wmClick(".next a");
 				wmClickLast("small a");
+				//ComicVine
+				wmClick(".paginate__item.next a");
+				//Google
+				wmClick("#pnnext");
+				//Lifehacker
+				wmClick(".the-paginator__arrow-icon--right");
+				//MailRu
+				wmClick(".icon_triangle_right");
+				//NarutoBase
+				wmClickLast("a.series-link");
+				//Ozon
+				wmClick(".b8n5 a");
+				//PWNews
+				wmClickLast(".catPages1 a");
 			}
 		},
 		false
