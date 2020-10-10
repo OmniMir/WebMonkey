bd5bb8f911a5da475f3de902d15287fa5b5aa506
kapsilon <kapsilon47@gmail.com>
2020-10-11 02:19:27 +0300

    Update Yandex_Hide_Elements

--- Yandex_Hide_Elements.user.js
+++ Yandex_Hide_Elements.user.js
@@ -7,6 +7,7 @@
 // @license		MIT
 // @match		https://yandex.ru/*
 // @match		https://*.yandex.ru/*
+// @match		https://yadi.sk/*
 // @require 	https://raw.githubusercontent.com/OmniMir/WebMonkey/master/lib.min.js
 // @grant		none
 // ==/UserScript==
@@ -17,6 +18,13 @@
 	window.addEventListener(
 		"load",
 		function () {
+			//Hide Promos
+			wmHide(".money-app-promo2");
+			wmHide(".offerwall-banner");
+			wmHide(".app-promo-banner");
+			wmHide(".b2b-banner");
+			//Hide Advertisements
+			wmHideAll("div[class*='TY__wrapper']");
 			//Hide Footer
 			wmHide("footer");
 			wmHide(".serp-footer__main");
