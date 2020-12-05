5ba2e50e94377a2a363e89c6372eece5ed401484
kapsilon <kapsilon47@gmail.com>
2020-12-05 16:17:25 +0300

    Add KGPortal_Hide_Elements

---
+++ KGPortal_Hide_Elements.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name		KGPortal Hide Elements
+// @namespace	https://vash.omnimir.ru/
+// @version		0.6
+// @description	Hide annoying elements
+// @author		kapsilon
+// @license		MIT
+// @match		https://kg-portal.ru/*
+// @require 	https://raw.githubusercontent.com/OmniMir/WebMonkey/master/lib.min.js
+// @grant		none
+// ==/UserScript==
+
+(function () {
+	"use strict";
+	//Start on page load to take effect
+	window.addEventListener(
+		"load",
+		function () {
+			//Hide Header
+			wmHide(".ten_topbar + div");
+
+			//Hide Blank Block
+			wmHideAll(".cough_block");
+			//Hide Social Garbage
+			wmHide(".section_social");
+			//Hide Promo Ads
+			wmHideAll(".kg_promo");
+
+			//Hide Donate Button
+			wmHideAll(".news_donate_author");
+			wmHideAll(".donate");
+			//Hide Sharing Button
+			wmHideAll(".sharing");
+			//Hide Comment Button
+			wmHide(".news_commentsjump");
+			//Hide Related Articles
+			wmHide(".section-related");
+
+			//Hide Footer
+			wmHide(".footer_wrap");
+		},
+		false
+	);
+})();
