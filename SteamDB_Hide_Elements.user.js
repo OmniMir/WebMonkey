8896b3dc7392d0ba0f7f5b534b190e4295f260da
kapsilon <kapsilon47@gmail.com>
2020-12-05 16:18:08 +0300

    Add SteamDB_Hide_Elements

---
+++ SteamDB_Hide_Elements.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name		SteamDB Hide Elements
+// @namespace	https://vash.omnimir.ru/
+// @version		0.6
+// @description	Hide annoying elements
+// @author		kapsilon
+// @license		MIT
+// @match		https://steamdb.info/*
+// @require 	https://raw.githubusercontent.com/OmniMir/WebMonkey/master/lib.min.js
+// @grant		none
+// ==/UserScript==
+
+(function () {
+	"use strict";
+	//Start on page load to take effect
+	window.addEventListener(
+		"load",
+		function () {
+			//Hide Game Preview
+			wmRemove(".hover");
+			//Hide Footer
+			wmHide(".footer");
+		},
+		false
+	);
+})();
