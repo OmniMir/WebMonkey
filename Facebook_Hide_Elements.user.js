aa19b56790ac03bf0f556d129cbce160e1173a25
kapsilon <kapsilon47@gmail.com>
2020-12-08 00:46:19 +0300

    Facebook_Hide_Elements use wmHideAllInStyle

--- Facebook_Hide_Elements.user.js
+++ Facebook_Hide_Elements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		Facebook Hide Elements
 // @namespace	https://vash.omnimir.ru/
-// @version		0.6
+// @version		0.8
 // @description	Hide annoying elements
 // @author		kapsilon
 // @license		MIT
@@ -12,22 +12,6 @@
 
 (function () {
 	"use strict";
-	//Start on page load to take effect
-	window.addEventListener(
-		"load",
-		function () {
-			//Hide Promote post Buttons
-			wmRemoveAll(".bq4bzpyk .bkfpd7mw.j83agx80");
-		},
-		false
-	);
-	//Going with page scroll
-	window.addEventListener(
-		"scroll",
-		function () {
-			//Hide Promote post Buttons
-			wmRemoveAll(".bq4bzpyk .bkfpd7mw.j83agx80");
-		},
-		false
-	);
+	//Hide Promote post Buttons
+	wmHideAllInStyle(".bq4bzpyk .bkfpd7mw.j83agx80");
 })();
