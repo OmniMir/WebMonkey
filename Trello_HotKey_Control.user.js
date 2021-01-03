c1f28cee9005d63a194b24b0e9b851cd65377186
kapsilon <kapsilon47@gmail.com>
2021-01-04 01:16:10 +0300

    UPDATE Trello_HotKey_Control

--- Trello_HotKey_Control.user.js
+++ Trello_HotKey_Control.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name		Trello HotKey Control
 // @namespace	https://vash.omnimir.ru/
-// @version		0.7
-// @description	Press Ctrl+F to Search
+// @version		0.8
+// @description	Ctrl+F to Search, Ctrl+S to Save
 // @author		kapsilon
 // @match		https://trello.com/*
 // @require 	https://raw.githubusercontent.com/OmniMir/WebMonkey/master/lib.min.js
@@ -11,40 +11,41 @@
 
 (function () {
 	"use strict";
-	window.addEventListener(
-		"keydown",
-		function (event) {
-			//Press Ctrl+F to Search at Board (without language switch bug)
-			if (event.code == "KeyF" && event.ctrlKey) {
-				//Disable Default Action
-				event.preventDefault();
+	//Start with Keydown
+	window.addEventListener("keydown", (event) => HotKeys(event), false);
 
-				//Click Invisible Menu Button
-				wmClick(".js-open-card-filter");
+	//Trello HotKey Control
+	function HotKeys(event) {
+		//Press Ctrl+F to Search at Board (without language switch bug)
+		if (event.code == "KeyF" && event.ctrlKey) {
+			//Disable Default Action
+			event.preventDefault();
 
-				//Show Search Menu if it was closed
-				wmClick(".js-show-sidebar");
-				//Focus to search bar if not autofocused
-				document.querySelector(".js-autofocus").focus();
-			}
+			//Click Invisible Menu Button
+			wmClick(".js-open-card-filter");
 
-			//Press Ctrl+S to Save Description at Card (shortcut to Ctrl+Enter)
-			else if (event.code == "KeyS" && event.ctrlKey) {
-				//Disable Default Action
-				event.preventDefault();
+			//Show Search Menu if it was closed
+			wmClick(".js-show-sidebar");
+			//Focus to search bar if not autofocused
+			document.querySelector(".js-autofocus").focus();
+		}
 
-				//Click Add Card Button
-				wmClick(".js-add-card.confirm");
-				//Click Create One Card
-				wmClick(".js-cancel.full");
-				//Click Save Card Button
-				wmClick(".js-save-edits.primary");
-				//Click Save Description Button
-				wmClick(".js-save-edit.confirm");
-				//Click Update Attachment
-				wmClick(".js-edit-attachment.primary");
-			}
-		},
-		false
-	);
+		//Press Ctrl+S to Save Description at Card (shortcut to Ctrl+Enter)
+		if (event.code == "KeyS" && event.ctrlKey) {
+			//Disable Default Action
+			event.preventDefault();
+
+			//Click Add Card Button
+			wmClick(".js-add-card.confirm");
+			//Click Create One Card Button
+			wmClick(".js-cancel.full");
+			//Click Save Card Button
+			wmClick(".js-save-edits.nch-button");
+			//Click Save Description Button
+			wmClick(".js-save-edit.confirm");
+			//Click Update Attachment Button
+			wmClick(".js-edit-attachment.nch-button");
+		}
+	}
 })();
+
