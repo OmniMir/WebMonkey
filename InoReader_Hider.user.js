2f03d0ed578c750212071c9c1860e7211140e2d9
kapsilon <kapsilon47@gmail.com>
2021-01-22 01:12:48 +0300

    ADD InoReader_Hider

---
+++ InoReader_Hider.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name		InoReader Hider
+// @namespace	https://vash.omnimir.ru/
+// @version		0.6
+// @description	Hide annoying elements
+// @author		kapsilon
+// @license		MIT
+// @match		https://www.inoreader.com/*
+// @require 	https://raw.githubusercontent.com/OmniMir/WebMonkey/master/lib.min.js
+// @grant		none
+// ==/UserScript==
+
+(function () {
+	"use strict";
+	//Start on page load to take effect
+	window.addEventListener("load", () => hideElements(), false);
+
+	//Hide Elements
+	function hideElements() {
+		//Hide Upgrade Button
+		wmHide("#sb_rp_upgrade");
+	}
+
+	wmAddNewStyle(`
+		.inno_toolbar_button_menu_item:nth-child(4) {
+			display: none;
+		}
+		.inno_toolbar_button_menu_item:nth-child(5) {
+			display: none;
+		}
+		.inno_toolbar_button_menu_item:nth-child(14) {
+			display: none;
+		}
+	`)
+})();
